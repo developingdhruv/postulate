990bf29eb7ef0012d2ee98f9175c3cd7889ad79f
Dhruv Agrawal <dhruvagrawal462@gmail.com>
2023-06-27 17:43:42 +0530

    first commit

---
+++ user profile/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.jumbotron>h1').addClass("animated fadeInLeftBig");
+    $('.jumbotron>h2').addClass("animated fadeInRightBig");
+    $('.jumbotron>p').addClass("animated fadeInDownBig");
+    
+    
+  });
